85a8b420dc263768ea1355d2ac42058cfd7fac0f
Brian McClain <brianmccln@yahoo.com>
2023-12-08 18:01:18 -0500

    add digits as search menu options

--- js/CocktailDB-API.js
+++ js/CocktailDB-API.js
@@ -6,15 +6,20 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTVWYZ"; // for making letter buttons (no "U" or "X")
 
 // cocktail keywords for making select options:
-const cocktailkKeywords = [ "Amaretto", "Beer", "Black", "Bloody", "Bourbon", "Fizz",
-    "Brandy", "Cherry", "Chocolate", "Coffee", "Coke", "Cream", "Gin", "Green","Iced", "Island", "Lemon", "Lime", "Liqueur", 
-    "Milk", "Orange", "Red", "Rum", "Salt", "Scotch", "Soda", "Sour", "Spiced", "Tea", "Tequila", "Vodka", "Whiskey", "Wine",
-    "Royal", "Punch", "Peach", "Cranberry", "Russian", "Captain", "Port", "Cooler", "Smash", "Cocktail", "Irish", "Jamaican",
-    "Tonic", "Slammer", "Apple", "Grape", "Cider", "High", "Long", "Caribbean", "Hot", "Almond", "Shot", "-", "Sweet", "Old",
-    "Banana", "Classic", "Electric", "Big", "Bermuda", "English", "Italian", "Smoothie", "Ginger", "Banana", "Papaya", "Frozen",
-    "Pineapple", "Kiwi", "Rose", "Blue", "Mojito", "Martini", "French", "Mango", "Negroni", "Golden", "Night", "Cold", 
-    "Mint", "Daiquiri", "Margarita", "Smash", "Shake", " and ", "Sling", "Berry", "Champagne", "Jack", "Hawaii", "Cordial", 
-    "Fruit", "Spring", "Spice", "Toddy", "Watermelon", "Lassi", "&" ];
+const cocktailkKeywords = [ 
+    "Amaretto", "Beer", "Black", "Bloody", "Bourbon", "Fizz", "Brandy", "Cherry", 
+    "Chocolate", "Coffee", "Coke", "Cream", "Gin", "Green", "Iced", "Island", "Lemon", 
+    "Lime", "Liqueur", "Milk", "Orange", "Red", "Rum", "Salt", "Scotch", "Soda", "Sour", 
+    "Spiced", "Tea", "Tequila", "Vodka", "Whiskey", "Wine", "Royal", "Punch", "Peach", 
+    "Cranberry", "Russian", "Captain", "Port", "Cooler", "Smash", "Cocktail", "Irish", 
+    "Jamaican", "Tonic", "Slammer", "Apple", "Grape", "Cider", "High", "Long", "Caribbean", 
+    "Hot", "Almond", "Shot", "-", "Sweet", "Old", "Banana", "Classic", "Electric", "Big", 
+    "Bermuda", "English", "Italian", "Smoothie", "Ginger", "Banana", "Papaya", "Frozen",
+    "Pineapple", "Kiwi", "Rose", "Blue", "Mojito", "Martini", "French", "Mango", "Negroni", 
+    "Golden", "Night", "Cold", "Mint", "Daiquiri", "Margarita", "Smash", "Shake", " and ", 
+    "Sling", "Berry", "Champagne", "Jack", "Hawaii", "Cordial", "Fruit", "Spring", "Spice", 
+    "Toddy", "Watermelon", "Lassi", ".", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+];
 
 cocktailkKeywords.sort(); // sort() array alphabetically, ascending order (A-Z)
 const btnBox = document.getElementById('letter-button-box'); // all letter btns go in here
